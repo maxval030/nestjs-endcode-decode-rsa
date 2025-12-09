3fa03db6e5c351514c9310bda5091a80f8018636
Supanat Lazy <supanat.y@lazyteams.com>
2025-12-10 03:25:23 +0700

    feat: 🎸 add validate to dto

--- src/dto/req/getEncrypt.req.dto.ts
+++ src/dto/req/getEncrypt.req.dto.ts
@@ -1,3 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class GetEncryptReqDto {
+  @ApiProperty({
+    default: 'This is data to be encrypted',
+    required: true,
+    example: 'This is data to be encrypted',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(2000)
   payload: string;
 }
